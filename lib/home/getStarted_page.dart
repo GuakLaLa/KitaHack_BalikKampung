d736c111abdc14d75f9120ba1e7fa470450b2193
GuakLaLa <zhiwei.tan2004@gmail.com>
2026-02-01 01:56:29 +0800

    Initialization of file and navigation page

---
+++ lib/home/getStarted_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GetstartedPage extends StatelessWidget{
+  const GetstartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
---
+++ lib/navigation.dart
@@ -0,0 +1,83 @@
+import 'package:floodsense/home/home_page.dart';
+import 'package:floodsense/map/map_page.dart';
+import 'package:floodsense/profile/profile_page.dart';
+import 'package:floodsense/report/report_page.dart';
+import 'package:flutter/material.dart';
+
+class NavigationPage extends StatefulWidget{
+  const NavigationPage({super.key});
+
+  @override
+  State<NavigationPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<NavigationPage> {
+  //this keep track of the selected index
+  int _selectedIndex = 0;
+
+  //this method updates the new selected index
+  void _navigateBottomBar(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  //the pages we have in the app
+  final List _pages = [
+    //home page
+    HomePage(),
+
+    //map page
+    MapPage(),
+
+    //report page
+    ReportPage(),
+
+    //profile page
+    ProfilePage()
+
+  ];
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      
+      appBar: AppBar(
+          title: Text("My App Bar"),
+          backgroundColor: Colors.greenAccent,
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.people),
+),
+          ],
+        ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Color.fromARGB(255, 165, 165, 165),
+        selectedItemColor: Colors.greenAccent,
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomBar, 
+        items: [
+        //home
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          label: 'Map',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.list_alt),
+          label: 'Report',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+
+      ]),  
+    );
+  }
+}
